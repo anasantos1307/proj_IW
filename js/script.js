0520f8bb0e2e7167e00fb7d4c1099ca94ae690e1
Ana Clara Martins dos Santos <100931641+anasantos1307@users.noreply.github.com>
2023-03-11 16:40:12 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,21 @@
+function pag0(){
+    window.location.href = "index.html"
+}
+
+function pag1(){
+
+    window.location.href = "sobre.html"
+}
+
+function pag2(){
+    window.location.href = "como.html"
+}
+
+function pag3(){
+    window.location.href = "outros.html"
+}
+
+function pag4(){
+    window.location.href = "contato.html"
+}
+
